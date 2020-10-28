ac33a6add5ada076115e655554c71e2df8d7f2cd
ssrikanth <ssrikanth@expedia.com>
2020-10-28 17:42:00 +1100

    add error handling for latitude

--- src/index.js
+++ src/index.js
@@ -11,11 +11,21 @@ class App extends React.Component {
             (position) => {
                 this.setState({lat: position.coords.latitude})
             },
-            (err) => console.log(err)
+            (err) => {
+                this.setState({errorMessage: err.message})
+            }
         );
     }
+
     render() {
-        return <div> Latitude : {this.state.lat}</div>
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div> Latitude : {this.state.lat}</div>;
+        }
+        return <div> Loading!!</div>;
     }
 }
 
